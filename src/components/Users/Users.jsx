8d15a0446a20fb3e7c76fdef16f71949a5942e82
vladover1 <anonim.rab@icloud.com>
2022-12-21 21:53:05 +0200

    users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
 import classes from './Users.module.scss'
 
-const Users = (props) => {
-    debugger
-    return (<div>
-        {props.users.map(u => <div key={u.id}>
+const Users = ({users, follow, unfollow, setUsers}) => {
+
+    if (users.length <3 ){
+    setUsers([{
+        id: 1,
+        photoURL: 'https://yt3.googleusercontent.com/ytc/AMLnZu_3irphACShro1kS-x6tx9aItHA2zyDJdcsESdO4A=s900-c-k-c0x00ffffff-no-rj',
+        followed: false,
+        fullName: 'Vlad O.',
+        status: 'i am a boss',
+        location: {city: 'Kiev', country: 'Ukraine'}
+    }, {
+        id: 2,
+        photoURL: 'https://yt3.googleusercontent.com/ytc/AMLnZu_3irphACShro1kS-x6tx9aItHA2zyDJdcsESdO4A=s900-c-k-c0x00ffffff-no-rj',
+        followed: true,
+        fullName: 'Nastya M.',
+        status: 'i am a boss too',
+        location: {city: 'Kharkiv', country: 'Ukraine'}
+    }, {
+        id: 3,
+        photoURL: 'https://yt3.googleusercontent.com/ytc/AMLnZu_3irphACShro1kS-x6tx9aItHA2zyDJdcsESdO4A=s900-c-k-c0x00ffffff-no-rj',
+        followed: false,
+        fullName: 'Volodya K.',
+        status: 'i am a boss too',
+        location: {city: 'Lviv', country: 'Ukraine'}
+    },])
+    }
+
+    return <div>
+        {users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoURL} alt=""/>
+                        <img className={classes.userPhoto} src={u.photoURL} alt="user avatar"/>
                     </div>
                     <div>
-                        <button>Follow</button>
+                       {u.followed
+                           ? <button onClick={() => unfollow(u.id)}>Followed</button>
+                           : <button onClick={() => follow(u.id)}>Unfollowed</button>}
                     </div>
                 </span>
-                <span>
+            <span>
                     <span>
                         <div>{u.fullName}</div><div>{u.status}</div>
                     </span>
@@ -22,7 +49,7 @@ const Users = (props) => {
                     </span>
                 </span>
         </div>)}
-    </div>);
+    </div>;
 };
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,28 +3,7 @@ const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
 
 let initialState = {
-    users: [{
-        id: 1,
-        photoURL: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Faws.amazon.com%2Fdeveloper%2Fcommunity%2Fheroes%2Fvlad-ionescu%2F&psig=AOvVaw3kIgUUcMPOjpHOHZvy00ZA&ust=1671221980558000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLCJypa5_PsCFQAAAAAdAAAAABAU',
-        followed: false,
-        fullName: 'Vlad O.',
-        status: 'i am a boss',
-        location: {city: 'Kiev', country: 'Ukraine'}
-    }, {
-        id: 2,
-        photoURL: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Faws.amazon.com%2Fdeveloper%2Fcommunity%2Fheroes%2Fvlad-ionescu%2F&psig=AOvVaw3kIgUUcMPOjpHOHZvy00ZA&ust=1671221980558000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLCJypa5_PsCFQAAAAAdAAAAABAU',
-        followed: true,
-        fullName: 'Nastya M.',
-        status: 'i am a boss too',
-        location: {city: 'Kharkiv', country: 'Ukraine'}
-    }, {
-        id: 3,
-        photoURL: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Faws.amazon.com%2Fdeveloper%2Fcommunity%2Fheroes%2Fvlad-ionescu%2F&psig=AOvVaw3kIgUUcMPOjpHOHZvy00ZA&ust=1671221980558000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLCJypa5_PsCFQAAAAAdAAAAABAU',
-        followed: false,
-        fullName: 'Volodya K.',
-        status: 'i am a boss too',
-        location: {city: 'Lviv', country: 'Ukraine'}
-    },]
+    users: []
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -50,7 +29,7 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS:
             return {...state, users: [...state.users, ...action.users]}
         default:
-            return {}
+            return state
     }
 }
 
