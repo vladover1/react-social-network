04ea4198b06b4c660a9b763e8f0c20fcf5e37006
vladover1 <anonim.rab@icloud.com>
2023-01-27 12:27:26 +0200

    life cycle metods

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,12 +5,12 @@ import userPhoto from "../../assets/images/users-alt.jpg"
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
+        componentDidMount(){
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 this.props.setUsers(response.data.items)
             })
-    }
+        }
+
 
     render() {
         return <div>
