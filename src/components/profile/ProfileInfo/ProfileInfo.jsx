6672347d3366f8f57f67234c5ad4d89f89ec6916
vladover1 <anonim.rab@icloud.com>
2023-02-06 13:27:47 +0200

    setState , local state

--- src/components/profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from "./Profileinfo.module.scss";
 import Preloader from "../../common/preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 
 const ProfileInfo = ({profile}) => {
@@ -12,14 +13,14 @@ const ProfileInfo = ({profile}) => {
     return (
         <div>
             <div>
-                <img
-                    className={classes.content__photo}
-                    src="https://cdn.corporate.walmart.com/dims4/WMT/f7a8313/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F7b%2F66%2F142c151b4cd3a19c13e1ca65f193%2Fbusinessfornature-banner.png"
-                    alt="background"/>
+                {/*<img*/}
+                {/*    className={classes.content__photo}*/}
+                {/*    src="https://cdn.corporate.walmart.com/dims4/WMT/f7a8313/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/quality/90/?url=https%3A%2F%2Fcdn.corporate.walmart.com%2F7b%2F66%2F142c151b4cd3a19c13e1ca65f193%2Fbusinessfornature-banner.png"*/}
+                {/*    alt="background"/>*/}
             </div>
             <div className={classes.descriptionBlock}>
                 <img src={profile.photos.small} alt='avatar'/>
-                ava+description
+                <ProfileStatus status={'hello'}/>
             </div>
         </div>
     );
